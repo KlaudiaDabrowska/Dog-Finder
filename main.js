0bd5bbb0525ee7eaccccab92ab6a17e79f135396
Klaudia Dabrowska <kosaa96@gmail.com>
2021-05-19 22:18:21 +0200

    Added eventListener to buttons

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
 const input=document.querySelector("input");
 const liElements=document.getElementsByTagName("li");
-// const breedBtn= document.querySelector("button");
-
 const ul=document.querySelector("ul");
+const shadow= document.querySelector(".shadow-dog");
+const breedBtn=document.getElementsByClassName("breed");
+const dogInfo=document.querySelector(".dog-info")
 
 
+const dog=()=>{
+    fetch(`https://dog.ceo/api/breeds/list`)
+    .then(res => res.json())
+    .then(data => {
+        const names= data.message;
+        names.forEach(name =>{
+            const li= document.createElement('li')
+            ul.appendChild(li);
+            const breedBtn=document.createElement("button");
+            breedBtn.className="breed";
+            li.appendChild(breedBtn);
+            breedBtn.textContent=name.charAt(0).toUpperCase() + name.slice(1);
+            })
+    })
+}
+
+dog()
 
 const searchDog= (e) => {
     const text=e.target.value.toLowerCase();
@@ -19,36 +37,27 @@ const searchDog= (e) => {
         }
         console.log(text)
     }
-
-    // liElements.forEach(li=> {
-    //     const dog= li.textContent
-    //     if (dog.toLowerCase().indexOf(text) !== -1){
-    //             li.style.display="block";
-    //     }
-    //     else{
-    //         li.style.display="none";
-    //     }
-    // })
- }
-
- input.addEventListener("input", searchDog)
-
-
- const dog=()=>{
-    fetch(`https://dog.ceo/api/breeds/list`)
-    .then(res => res.json())
-    .then(data => {
-        const names= data.message;
-        names.forEach(name =>{
-            const li= document.createElement('li')
-            ul.appendChild(li);
-            const breedBtn=document.createElement("button");
-            breedBtn.className="breed";
-            li.appendChild(breedBtn);
-            breedBtn.textContent=name.charAt(0).toUpperCase() + name.slice(1);
-        })
-
- })
 }
 
-dog()
+input.addEventListener("input", searchDog)
+
+
+const openInfo=()=>{
+    shadow.style.display="flex";
+}
+
+document.body.addEventListener("click", function(e){
+    if(e.target.className === "breed"){
+        openInfo();
+    };
+});
+
+
+
+const closeInfo=()=>{
+    shadow.style.display="none";
+}
+
+window.addEventListener("click", (e)=>{
+    e.target===shadow ? closeInfo() : false
+});
