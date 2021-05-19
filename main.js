b35d378d07f84a5d5de155eb75c9b549355d1c22
Klaudia Dabrowska <kosaa96@gmail.com>
2021-05-19 20:49:33 +0200

    Deleted folders with breeds, added function to get data from api

--- main.js
+++ main.js
@@ -1,21 +1,54 @@
 const input=document.querySelector("input");
-const liElements=document.querySelectorAll("li");
+const liElements=document.getElementsByTagName("li");
+// const breedBtn= document.querySelector("button");
+
+const ul=document.querySelector("ul");
 
 
 
 const searchDog= (e) => {
     const text=e.target.value.toLowerCase();
     
-
-    liElements.forEach(li=> {
-        const dog= li.textContent
+    for(let i=0; i<liElements.length; i++){
+        const dog= liElements[i].innerText;
         if (dog.toLowerCase().indexOf(text) !== -1){
-                li.style.display="block";
+                liElements[i].style.display="block";
         }
         else{
-            li.style.display="none";
+            liElements[i].style.display="none";
         }
-    })
+        console.log(text)
+    }
+
+    // liElements.forEach(li=> {
+    //     const dog= li.textContent
+    //     if (dog.toLowerCase().indexOf(text) !== -1){
+    //             li.style.display="block";
+    //     }
+    //     else{
+    //         li.style.display="none";
+    //     }
+    // })
  }
 
  input.addEventListener("input", searchDog)
+
+
+ const dog=()=>{
+    fetch(`https://dog.ceo/api/breeds/list`)
+    .then(res => res.json())
+    .then(data => {
+        const names= data.message;
+        names.forEach(name =>{
+            const li= document.createElement('li')
+            ul.appendChild(li);
+            const breedBtn=document.createElement("button");
+            breedBtn.className="breed";
+            li.appendChild(breedBtn);
+            breedBtn.textContent=name.charAt(0).toUpperCase() + name.slice(1);
+        })
+
+ })
+}
+
+dog()
